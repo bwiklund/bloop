7c91bcae72e61ec217901a1855d62d4e752aab10
Ben Wiklund <ben@daisyowl.com>
2015-05-01 14:29:33 -0700

    hashing files like git

--- main.hs
+++ main.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Bloop where
 
-import Crypto.Hash.SHA512 (hashlazy)
-import qualified Data.ByteString.Lazy as Lazy
+import Crypto.Hash.SHA1 (hashlazy)
+import qualified Data.ByteString.Lazy.Char8 as Lazy
 
 main = fileSum "main.hs" >>= putStrLn . show
 
-fileSum path = fmap (hashlazy) (Lazy.readFile path)
+fileSum path = do
+  contents <- Lazy.readFile path
+  let len = Lazy.length contents
+  return $ hashlazy $ Lazy.concat ["blob ", Lazy.pack $ show len, "\0", contents]
+
