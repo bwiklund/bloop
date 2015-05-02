4e2a1ab3b7242eaf9b74f45f67bf6ea8bb2d1d46
Ben Wiklund <ben@daisyowl.com>
2015-05-01 17:04:24 -0700

    hex string, posix file size

--- main.hs
+++ main.hs
@@ -4,11 +4,14 @@ module Bloop where
 
 import Crypto.Hash.SHA1 (hashlazy)
 import qualified Data.ByteString.Lazy.Char8 as Lazy
+import Data.ByteString.Builder (toLazyByteString, byteStringHex)
+import qualified System.Posix as Posix
 
-main = fileSum "main.hs" >>= putStrLn . show
+main = fileSum "main.hs" >>= print
 
 fileSum path = do
   contents <- Lazy.readFile path
-  let len = Lazy.length contents
-  return $ hashlazy $ Lazy.concat ["blob ", Lazy.pack $ show len, "\0", contents]
-
+  stat <- Posix.getFileStatus path
+  let len = Posix.fileSize stat
+      headerAndContents = ["blob ", Lazy.pack $ show len, "\0", contents]
+  return $ toLazyByteString $ byteStringHex $ hashlazy $ Lazy.concat headerAndContents
