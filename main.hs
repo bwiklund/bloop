563c5cc961f20a22bfd6bb4ddaccb48bf66e5cb0
Ben Wiklund <ben@daisyowl.com>
2015-05-01 17:18:22 -0700

    refactor

--- main.hs
+++ main.hs
@@ -5,13 +5,12 @@ module Bloop where
 import Crypto.Hash.SHA1 (hashlazy)
 import qualified Data.ByteString.Lazy.Char8 as Lazy
 import Data.ByteString.Builder (toLazyByteString, byteStringHex)
-import qualified System.Posix as Posix
 
 main = fileSum "main.hs" >>= print
 
-fileSum path = do
-  contents <- Lazy.readFile path
-  stat <- Posix.getFileStatus path
-  let len = Posix.fileSize stat
-      headerAndContents = ["blob ", Lazy.pack $ show len, "\0", contents]
-  return $ toLazyByteString $ byteStringHex $ hashlazy $ Lazy.concat headerAndContents
+fileSum path = fmap blobHash $ Lazy.readFile path
+
+blobHash blob = toHexHash headerAndContents
+  where toHexHash = toLazyByteString . byteStringHex . hashlazy
+        len = Lazy.length blob
+        headerAndContents = Lazy.concat ["blob ", Lazy.pack $ show len, "\0", blob]
