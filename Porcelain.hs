9fed4aa6ba323675ba739505faf3fcbfdfe391ae
Ben Wiklund <ben@daisyowl.com>
2015-05-03 15:40:09 -0700

    cat command

--- Porcelain.hs
+++ Porcelain.hs
@@ -13,6 +13,10 @@ processArgs [] = putStrLn usage
 processArgs (command:args)
   | command == "init" = initRepo
   | command == "mark" = markObjectCommand args >>= putStrLn . unlines
+  | command == "cat"  = catObjectCommand args >>= Lazy.putStrLn
   | otherwise         = putStrLn usage
 
 markObjectCommand args = mapM Lazy.readFile args >>= (mapM storeObject)
+
+catObjectCommand args = readObject hash
+  where hash = head args
